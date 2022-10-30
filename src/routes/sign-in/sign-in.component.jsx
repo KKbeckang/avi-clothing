32ef3244347849a125a420a3d8079fb339bdb54c
KKbeckang <beckang@bu.edu>
2022-10-30 04:58:40 -0400

    Add Signup Form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,20 @@
-import { auth,signInWithGooglePopup,createUserDocumentFromAuth,signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
+import { signInWithGooglePopup,createUserDocumentFromAuth,signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
 
-    useEffect( () => {
+    // useEffect( () => {
 
-        const loadData = async() => {
-            const response = await getRedirectResult(auth);
-            if(response) {
-                const userDocRef = await createUserDocumentFromAuth(response.user);
-            }
-        }
-        loadData()
-    },[]);
+    //     const loadData = async() => {
+    //         const response = await getRedirectResult(auth);
+    //         if(response) {
+    //             const userDocRef = await createUserDocumentFromAuth(response.user);
+    //         }
+    //     }
+    //     loadData()
+    // },[]);
     
     const logGoogleUser = async() => {
         const {user}= await signInWithGooglePopup();
@@ -25,8 +26,9 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}> Sign in with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}> 
-            Sign in with Google GOogleredirect</button>
+            <SignUpForm/>
+            {/* <button onClick={signInWithGoogleRedirect}> 
+            Sign in with Google GOogleredirect</button> */}
         </div>
     );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,67 +1,62 @@
-import {initializeApp} from "firebase/app";
+import { initializeApp } from "firebase/app";
 import {
-    getAuth, 
-    signInWithRedirect,
-    signInWithPopup,
-    GoogleAuthProvider
-} from "firebase/auth"
-import{
-    getFirestore,
-    doc,
-    getDoc,
-    setDoc} from "firebase/firestore";
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyChZYNCyIe3_Tx6ajTILzgiDo82VSIiuUQ",
-    authDomain: "avi-clothing-db-71b7e.firebaseapp.com",
-    projectId: "avi-clothing-db-71b7e",
-    storageBucket: "avi-clothing-db-71b7e.appspot.com",
-    messagingSenderId: "579136068427",
-    appId: "1:579136068427:web:6614f07254b0cc1ac926ce"
-  };
-  
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyChZYNCyIe3_Tx6ajTILzgiDo82VSIiuUQ",
+  authDomain: "avi-clothing-db-71b7e.firebaseapp.com",
+  projectId: "avi-clothing-db-71b7e",
+  storageBucket: "avi-clothing-db-71b7e.appspot.com",
+  messagingSenderId: "579136068427",
+  appId: "1:579136068427:web:6614f07254b0cc1ac926ce",
+};
 
-  const   googleProvider = new GoogleAuthProvider();
-  
-  googleProvider.setCustomParameters({
-    prompt:"select_account"
-  });
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
 
-  export const auth = getAuth();
-  //gernearl providers we could use Faceboo,othe sign in 
-  export const signInWithGooglePopup = () => signInWithPopup(auth,  googleProvider);
+const googleProvider = new GoogleAuthProvider();
 
-  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
 
+export const auth = getAuth();
+//gernearl providers we could use Faceboo,othe sign in
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
 
-  export const db = getFirestore();
-  export const createUserDocumentFromAuth = async(userAuth) => {
-    const userDocRef= doc(db,'users',userAuth.uid);
-    console.log(userDocRef);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
-    const userSnapshot = await getDoc(userDocRef)
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
 
-    //if user data exists
-    if(!userSnapshot.exists()){
-        const {displayName,email} = userAuth;
-        const createdAt = new Date();
+  const userSnapshot = await getDoc(userDocRef);
 
-        try{
-            await setDoc(userDocRef,{
-                displayName,
-                email,
-                createdAt
-                });
-        }catch(error){
-            console.log('error creating the user', error.message);
-        }
-        
+  //if user data exists
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
     }
-    return userDocRef;
-    //if user data does not exist
-    // create data from snapshot
-    // return userDocRef
   }
+  return userDocRef;
+  //if user data does not exist
+  // create data from snapshot
+  // return userDocRef
+};
